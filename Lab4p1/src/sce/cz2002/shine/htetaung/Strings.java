4e32c0375859950792035119349afc61966d2684
Htet Aung Shine <has.htetaungshine1994@gmail.com>
2016-08-21 19:59:34 +0800

    Added Lab4 (WIP)

---
+++ Lab4p1/src/sce/cz2002/shine/htetaung/Strings.java
@@ -0,0 +1,15 @@
+package sce.cz2002.shine.htetaung;
+
+import java.util.Scanner;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		String[] stringList;
+		int size;
+		Scanner sc = new Scanner(System.in);
+		System.out.print ("\nHow many strings do you want to sort? ");
+		size = sc.nextInt();
+		stringList = new String[size];
+	}
+}
