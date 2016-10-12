f14617d02e2713c8d720ba6371beec82e37a7fc8
Htet Aung Shine <has.htetaungshine1994@gmail.com>
2016-10-13 00:20:58 +0800

    Lab4 done

--- Lab4p1/src/sce/cz2002/shine/htetaung/Strings.java
+++ Lab4p1/src/sce/cz2002/shine/htetaung/Strings.java
@@ -10,6 +10,19 @@ public class Strings {
 		Scanner sc = new Scanner(System.in);
 		System.out.print ("\nHow many strings do you want to sort? ");
 		size = sc.nextInt();
+		sc.nextLine();
 		stringList = new String[size];
+		for(int i = 0; i < size; i++)
+		{
+			System.out.print("\nEnter your string no. " + (i+1) + " ");
+			stringList[i] = sc.nextLine();
+		}
+		
+		Sorting.selectionSort(stringList);
+		System.out.println ("\nYour stringss in sorted order...");
+		
+		for(int i = 0; i < size; i++)
+			System.out.println((i+1) + ". " + stringList[i]);
+		System.out.println();
 	}
 }
